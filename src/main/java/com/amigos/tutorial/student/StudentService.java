c1044c6544a6a07068540e01f5dcfd6dab8f32e3
deluxesande <deluxesande@gmail.com>
2023-12-20 18:39:07 +0300

    basic student API with Controller and service layer

---
+++ src/main/java/com/amigos/tutorial/student/StudentService.java
@@ -0,0 +1,12 @@
+package com.amigos.tutorial.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(1L, "John", "john@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21));
+    }
+}
